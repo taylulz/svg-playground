51b87c1d2630dbf7d856bbe22489db5bf9f56625
taylulz <taylor.delph@eyecuelab.com>
2021-10-10 19:31:56 -0700

    save changes in new branch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import { ReactComponent as Logo } from '../img/logo.svg';
 import { Container } from 'react-bootstrap';
 
+const item = document.querySelector("SvgLogo");
+// console.log(item.getTotalLength())
+console.log(Logo.Container)
+
 function Home() {
   return (
     <>
       <Container>
         <Logo className="App-logo" alt="logo"/>
+
       </Container>
     </>
   );
